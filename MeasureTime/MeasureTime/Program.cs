89754b38250f68369d0ff74301807bd8ac3bfc2a
enrique7mc <enrique7mc@gmail.com>
2015-08-30 22:25:37 -0500

    Initial commit

---
+++ MeasureTime/MeasureTime/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasureTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            using(var timeBlock = new TimeMeasurementBlock(stopwatch))
+            {
+                for(int i = 0; i < 300000; i++)
+                {
+                    IsPrime(i);
+                }
+
+                Console.WriteLine(timeBlock.Stopwatch.ElapsedMilliseconds);                
+            }
+        }
+
+        private static bool IsPrimeSlow(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsPrime(int n)
+        {
+            if (n == 2 || n == 3)
+            {
+                return true;
+            }
+
+            if (n % 2 == 0 || n % 3 == 0)
+            {
+                return false;
+            }
+
+            int i = 5;
+            int w = 2;
+            while (i * i <= n)
+            {
+                if (n % 2 == 0)
+                {
+                    return false;
+                }
+
+                i += w;
+                w = 6 - w;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ MeasureTime/MeasureTime/TimeMeasurementBlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasureTime
+{
+    public class TimeMeasurementBlock : IDisposable
+    {
+        public Stopwatch Stopwatch { get; private set; }
+
+        public TimeMeasurementBlock(Stopwatch stopwatch)
+        {
+            Stopwatch = stopwatch;
+            if(Stopwatch != null)
+            {
+                Stopwatch.Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            if(Stopwatch != null)
+            {
+                Stopwatch.Stop();
+            }
+        }
+    }
+}
